e57f0a9d6b18f972b53bfe43c934f2c3b0684562
amutehjnr <mustaeenms@gmail.com>
2025-07-20 08:39:05 +0100

    Update WrappedTokenBridge.sol

--- contracts/WrappedTokenBridge.sol
+++ contracts/WrappedTokenBridge.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { SameDex } from "./SameDex.sol";
@@ -17,26 +14,10 @@ interface IERC20Burnable is IERC20 {
     function burnFrom(address from, uint256 amount) external;
 }
 
-contract WrappedTokenBridge is
-    Initializable,
-    UUPSUpgradeable,
-    OwnableUpgradeable,
-    SameDex
-{
+contract WrappedTokenBridge is SameDex {
     using SafeERC20 for IERC20;
 
-    address public nativeToken;
-
-    function initialize(
-        address _endpoint,
-        address _delegate,
-        address _nativeToken
-    ) public initializer {
-        __Ownable_init(msg.sender);
-        __UUPSUpgradeable_init();
-        __SameDex_init(_endpoint, _delegate);
-        nativeToken = _nativeToken;
-    }
+    constructor(address _endpoint, address _delegate) SameDex(_endpoint, _delegate) {}
 
     function bridge(
         uint32 dstEid,
@@ -44,18 +25,12 @@ contract WrappedTokenBridge is
         uint256 amount,
         address receiver
     ) external payable {
-        if (token == address(0)) {
-            require(msg.value == amount, "Native value mismatch");
-        } else {
-            IERC20Burnable(token).burnFrom(msg.sender, amount);
-        }
-
-        bytes memory payload = abi.encode(token, amount, receiver);
+        IERC20Burnable(token).burnFrom(msg.sender, amount);
 
         bytes memory options = OptionsBuilder.newOptions();
-        options = OptionsBuilder.addExecutorLzReceiveOption(options, 200_000, 0);
+        options = OptionsBuilder.addExecutorLzReceiveOption(options, 200_000, 0); // adjust gas if needed
 
-        _lzSend(dstEid, payload, options, _quote(dstEid, payload, options, false), msg.sender);
+        _bridge(dstEid, token, amount, receiver, options);
 
         emit Bridged(dstEid, token, amount, receiver);
     }
@@ -66,19 +41,8 @@ contract WrappedTokenBridge is
         uint256 amount,
         address to
     ) internal override {
-        if (token == address(0)) {
-            (bool sent, ) = to.call{value: amount}("");
-            require(sent, "Native token transfer failed");
-        } else {
-            IERC20Mintable(token).mint(to, amount);
-        }
+        IERC20Mintable(token).mint(to, amount);
     }
 
-    function setNativeToken(address _nativeToken) external onlyOwner {
-        nativeToken = _nativeToken;
-    }
-
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
-
-    receive() external payable {}
+    
 }
